fa330ef55403b8debd627974d088fab7e93ae9e0
inlend <128955464+inlend@users.noreply.github.com>
2024-04-05 11:11:38 +0200

    Add files via upload

--- portfolio/descriptions.js
+++ portfolio/descriptions.js
@@ -1,6 +1,6 @@
 var description = [
 
-	'ⓘ Eleuzina <br> <br> Posters for events of a Slovac cultural center', 
+	'ⓘ Eleuzina <br> <br> Posters for events of a Slovac cultural center.', 
 
 	'ⓘ Animation Reel <br> <br> 2D and 3D animation samples including excercises done during an exchange semester at the Kyoto University of the Arts, showcasing an intro for an imaginary TV show.  <br> <br> Find the full video by scrolling down.', 
 
@@ -14,7 +14,7 @@ var description = [
 
 	'ⓘ Rokko Meets Art Open Call <br> <br> Flyer and Instagram posts about an open call for an internship taking place in the context of the Rokko Meets Art festival at Mt. Rokko, north of Kobe.', 
 
-	'ⓘ Bells End the Day <br> <br> Installation with two video channels, rendered in realtime, linking together the history of walking rituals, the work of the late conceptual artist Hanne Darboven and simulations as a way of performing and keeping track of time.  <br> <br> One screen depicts a circular walk through an ever changing environment, never returning to the same place twice, the other an algorithm which calculates and draws out a new value for each date in the calendar.  <br> <br> Find an excerpt of it <a href="https://vimeo.com/914743422" target="_blank"> here!</a>', 
+	'ⓘ Missing Love Book Design <br> <br> Design for the appearance and layout of a book collecting semester projects from a class led by Kyung-hwa Choi-ahoi. <br> <br> The design was done in collaboration with Alba Hiedras and Oleksandra Pshenychna.', 
 
 	'ⓘ >Acting< Zine <br> <br> A small risograph zine about acting, the Meisner technique and being calm. <br> <br> Made during a workshop led by Stavros Gialamidis.', 
 
@@ -22,7 +22,7 @@ var description = [
 
 	'ⓘ Glare <br> <br> 13 minute animation made during the CTR Substructured Loss residency. It follows an anonymous player through the mysterious world of an imaginary point-and-click game as it slowly untethers. <br><br> It was shown as part of the exhibition Of Lines and Familiar Thresholds in 2019.', 
 
-	'ⓘ Missing Love Book Design <br> <br> Design for the appearance and layout of a book collecting semester projects from a class led by Kyung-hwa Choi-ahoi. <br> <br> The design was done in collaboration with Alba Hiedras and Oleksandra Pshenychna.',
+	'ⓘ Bells End the Day <br> <br> Installation with two video channels, rendered in realtime, linking together the history of walking rituals, the work of the late conceptual artist Hanne Darboven and simulations as a way of performing and keeping track of time.  <br> <br> One screen depicts a circular walk through an ever changing environment, never returning to the same place twice, the other an algorithm which calculates and draws out a new value for each date in the calendar.  <br> <br> Find an excerpt of it <a href="https://vimeo.com/914743422" target="_blank"> here!</a>',
 
 	'ⓘ Screen Memory <br> <br> This two channel video installation investigates the role of screens in processes of remembering, forgetting and missing. <br> <br> Multiple narratives surrounding themes of loss, Greek mythology, language history and cinema are interwoven to depict both personal as well as collective forms of ‘screen memories’, and their potential to both destabilise and create new meaning. ',
 
