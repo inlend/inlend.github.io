070cf68640882661235efff0321db084a810e8f4
inlend <128955464+inlend@users.noreply.github.com>
2024-04-05 08:50:53 +0200

    Add files via upload

--- portfolio/descriptions.js
+++ portfolio/descriptions.js
@@ -1,40 +1,42 @@
 var description = [
 
-	'ⓘ Eleuzina <br> <br> posters for events of a Slovac cultural center', 
+	'ⓘ Eleuzina <br> <br> Posters for events of a Slovac cultural center', 
 
-	'ⓘ Animation Reel <br> <br> 2D and 3D animation samples including excercises done during an exchange semester at the Kyoto University of the Arts, showcasing an intro for an imaginary TV show. You can scroll down for the full video!', 
+	'ⓘ Animation Reel <br> <br> 2D and 3D animation samples including excercises done during an exchange semester at the Kyoto University of the Arts, showcasing an intro for an imaginary TV show.  <br> <br> Find the full video by scrolling down.', 
 
-	'ⓘⓘⓘ <br> <br> CGI Illustrations, top one based on God`s List of Liquids by Anne Carson, bottom one as part of AR exhibition of the Wrong Biennale', 
+	'ⓘ CGI Illustrations <br> <br> The upper Illustration is based on God`s List of Liquids by Anne Carson, the bottom one was part of AR exhibition of the Wrong Biennale', 
 
-	'4', 
+	'ⓘ Plan B Bookfair <br> <br> Following the cancellation of the Leipzig Book Fair of 2022, our class decided to organise an alternative event, showcasing the work of design-, book art- and illustration students throughout Germany. <br> <br> I participated in designing, modelling and building the exhibition modules and prints as well as planning the spacial layout of the event.', 
 
-	'5', 
+	'ⓘ Leana Ardeleanu Website <br> <br> A portfolio-website, logo and splash screen animation for Berlin based make-up artist Leana Ardeleanu. <br> <br> You can find it <a href="https://leana-ardeleanu.com/" target="_blank"> here! </a>', 
 
-	'6', 
+	'ⓘ 2024 poster competition <br> <br> Entry for 2024’s competition to design the poster for the open days of the weißensee kunsthochschule berlin, employing rebuses as a way to communicate the content of the poster and engage viewers. <br> <br> It won 3rd place in the competition. ', 
 
-	'7', 
+	'ⓘ Rokko Meets Art Open Call <br> <br> Flyer and Instagram posts about an open call for an internship taking place in the context of the Rokko Meets Art festival at Mt. Rokko, north of Kobe.', 
 
-	'8', 
+	'ⓘ Bells End the Day <br> <br> Installation with two video channels, rendered in realtime, linking together the history of walking rituals, the work of the late conceptual artist Hanne Darboven and simulations as a way of performing and keeping track of time.  <br> <br> One screen depicts a circular walk through an ever changing environment, never returning to the same place twice, the other an algorithm which calculates and draws out a new value for each date in the calendar.  <br> <br> Find an excerpt of it <a href="https://vimeo.com/914743422" target="_blank"> here!</a>', 
 
-	'9', 
+	'ⓘ >Acting< Zine <br> <br> A small risograph zine about acting, the Meisner technique and being calm. <br> <br> Made during a workshop led by Stavros Gialamidis.', 
 
-	'10', 
+	'ⓘ Writing Eat Up Language <br> <br> Class project of a  brochure on the origin of writing and the disconnect between heard and written language. Custom font adaption of “Visible Speech”, a notation system of mouth sounds first proposed by Alexander Melville Bell.', 
 
-	'11', 
+	'ⓘ Glare <br> <br> 13 minute animation made during the CTR Substructured Loss residency. It follows an anonymous player through the mysterious world of an imaginary point-and-click game as it slowly untethers. <br><br> It was shown as part of the exhibition Of Lines and Familiar Thresholds in 2019.', 
 
-	'12',
+	'ⓘ Missing Love Book Design <br> <br> Design for the appearance and layout of a book collecting semester projects from a class led by Kyung-hwa Choi-ahoi. <br> <br> The design was done in collaboration with Alba Hiedras and Oleksandra Pshenychna.',
 
-	'13',
+	'ⓘ Screen Memory <br> <br> This two channel video installation investigates the role of screens in processes of remembering, forgetting and missing. <br> <br> Multiple narratives surrounding themes of loss, Greek mythology, language history and cinema are interwoven to depict both personal as well as collective forms of ‘screen memories’, and their potential to both destabilise and create new meaning. ',
 
-	'14',
+	'ⓘ Graphics and Illustration <br> <br> Various works from personal projects, student exercises and open calls.',
 
-	'15',
+	'ⓘ About <br> <br> ::::::::::::::::::::::::::<br> :::::::::@@@@@@@@:::::::::<br>::::::@@@@@@@@@@@@@@::::::<br>::::@@@@@@@@@@@@@@@@@@::::<br>:::@@@::::@@@@@@::::@@@:::<br>::@@@@::::@@@@@@::::@@@@::<br>::@@@@@@@@@@::@@@@@@@@@@::<br>::@@@@@@@@@@::@@@@@@@@@@::<br>::@@@@@@@@@@@@@@@@@@@@@@::<br>:::@@@@@:::@@@@:::@@@@@:::<br>::::@@@@@::::::::@@@@@::::<br>::::::@@@@@@@@@@@@@@::::::<br>:::::::::@@@@@@@@:::::::::<br>::::::::::::::::::::::::::<br>',
 
-	'16',
+	'ⓘ XXX <br> <br>',
 
-	'17',
+	'ⓘ XXX <br> <br>',
 
-	'18',
+	'ⓘ XXX <br> <br>',
+
+	'ⓘ XXX <br> <br>',
 
 
 	];
